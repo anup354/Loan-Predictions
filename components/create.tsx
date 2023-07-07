bed749bf05425b93093d7422dd42d59ba8ed2edb
Anup Kasula <anup.kasula@elscript.com>
2023-07-07 18:21:01 +0545

    add login page

--- components/create.tsx
+++ components/create.tsx
@@ -36,12 +36,14 @@ const Create = () => {
      <Head>
         <title>Sign Up | Loan Predictor</title>
       </Head>
-      <div className="max-md:pt-0 max-lg:pt-0 max-sm:pt-0 max-w-screen-3xl mx-auto">
-        <div className="px-20   py-10">
-          <div className="flex  ">
-            <div className="flex-1 bg-primary rounded-l-md px-10 py-10 text-white">
-              <div className="text-4xl font-semibold ">Signup</div>
-              <div className="py-5">Sign up to create your account.</div>
+      <div>
+        <div className="px-20 max-lg:px-10 max-sm:px-5  py-10">
+          <div className="flex max-lg:flex-col ">
+            <div className="flex-1  bg-primary rounded-l-md px-10 py-10 text-white">
+              <div className="text-4xl font-semibold ">Sign up</div>
+              <div className="py-5">
+              Sign up to create your account
+              </div>
               <div className="">Already member of Loan Predictor</div>
               <Link href="/login">
                 {" "}
@@ -49,6 +51,10 @@ const Create = () => {
                   Login
                 </button>
               </Link>
+              {/* <div className="">New here ? Join softsaro</div>
+           <Link href="/signup">  <button className="px-6 py-1 border-2 rounded-md my-5 hover:bg-white hover:text-primary text-lg font-medium">
+                Sign up
+              </button></Link>  */}
               <div className=" flex gap-10  text-hero text-slate-200 pt-5">
                 <Link href="https://www.facebook.com/softsaronepal">
                   {" "}
@@ -101,6 +107,7 @@ const Create = () => {
                   <div className="relative">
                     <label htmlFor="password">Password</label> <br />
                     <input
+                      minLength={8}
                       type={isvisible ? "text" : "password"}
                       placeholder="Password"
                       {...register("password", {
@@ -118,11 +125,11 @@ const Create = () => {
                     >
                       {isvisible ? (
                         <span className="">
-                          <MdOutlineVisibility/>
+                          <MdOutlineVisibility />
                         </span>
                       ) : (
                         <span className="">
-                          <MdOutlineVisibilityOff/>
+                          <MdOutlineVisibilityOff />
                         </span>
                       )}
                     </div>
@@ -130,15 +137,14 @@ const Create = () => {
                 </div>
                 {errors.password && (
                   <span className="text-red-500 text-sm">
-                    {console.log(errors)}
                     {errors.password.message}
                   </span>
                 )}
-                <div className="pt-5">
+                <div className=" pt-5">
                   <div className="relative">
-                    <label htmlFor="confirmPassword">Confirm Password</label>{" "}
-                    <br />
+                    <label htmlFor="confirmPassword">Confirm Password</label> <br />
                     <input
+                      minLength={8}
                       type={isConfirmVisible ? "text" : "password"}
                       placeholder="Confirm Password"
                       {...register("confirmPassword", {
@@ -149,7 +155,7 @@ const Create = () => {
                       })}
                       className={
                         errors?.confirmPassword
-                          ? "text-xs border p-5 rounded mt-3 border-red-600 bg-red-50 w-full focus:outline-none"
+                          ? "text-xs border p-5 rounded mt-3 border-red-600 bg-red-50 w-full   focus:outline-none"
                           : "justify-center items-center border px-4 py-4 w-full rounded-md outline-none text-sm mt-3 focus:ring-1 focus:ring-primary"
                       }
                     />
@@ -159,11 +165,11 @@ const Create = () => {
                     >
                       {isConfirmVisible ? (
                         <span className="">
-                          <MdOutlineVisibility/>
+                          <MdOutlineVisibility />
                         </span>
                       ) : (
                         <span className="">
-                          <MdOutlineVisibilityOff/>
+                          <MdOutlineVisibilityOff />
                         </span>
                       )}
                     </div>
@@ -174,18 +180,17 @@ const Create = () => {
                     {errors.confirmPassword.message}
                   </span>
                 )}
+                <Link href="">
+                  <div className="underline text-xs py-5 text-primary text-center">
+                    Having trouble logging in?
+                  </div>
+                </Link>
                 <button
-                  className="w-full my-5 font-medium text-lg text-white bg-primary py-3 rounded-md hover:bg-white hover:text-primary border hover:border-primary"
+                  className="w-full font-medium text-lg text-white bg-primary py-3 rounded-md hover:bg-white hover:text-primary border hover:border-primary"
                   type="submit"
                 >
-                  Sign Up
+                  Submit
                 </button>
-                <div className=" text-xs  text-primary justify-center flex">
-                  <div>I&apos;m already a member. </div>
-                  <Link href="/login">
-                    <div className="underline"> Log in</div>
-                  </Link>
-                </div>
               </form>
             </div>
           </div>
